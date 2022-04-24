37d237b5ad796740f561594d7b7c46319f633705
Muhammad Arslan <muhammadarslanishtiaq@gmail.com>
2022-04-24 19:14:54 +0500

    bootstrap removed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div className="section-01">
+        <div className="section-01-inner">
+          <div className="col-6">
+            <p>Name.It</p>
+          </div>
+          <div className="col-6 align-end">
+            <p>Contattaci</p>
+          </div>
+        </div>
+      </div>
+      <div className="section-02">
+        <div className="section-02-inner">
+          <h1>Hello World</h1>
+          <button type="button" class="button">
+            This is a button
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
